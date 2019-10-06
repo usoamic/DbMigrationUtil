adcf7971bbb69a3f433284d3b075c01a05226490
primys <a2uso@tuta.io>
2019-10-06 13:43:06 -0700

    Add GoogleAuthenticator dependencies

--- run.php
+++ run.php
@@ -7,6 +7,7 @@ require_once("php/libs/base/EncryptionBase.php");
 require_once("php/libs/EncryptionLegacyUtil.php");
 require_once("php/libs/EncryptionUtil.php");
 require_once("php/libs/DBClass.php");
+require_once("php/libs/gauthenticator/GoogleAuthenticator.php");
 require_once("php/daemon/jsonRPCClient.php");
 require_once("php/daemon/RPC.php");
 require_once ('MigrationUtil.php');
